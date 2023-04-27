1faf2e9c844a8f55b7ee8f3f4d924bcf8e1fafff
Will D <pleplu01@gmail.com>
2023-04-26 23:53:17 -0400

    Updated endpoints and responses
    Added new endpoints to API

--- index.js
+++ index.js
@@ -1,62 +1,78 @@
 const express = require('express'),
-    morgan = require('morgan');
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
 
 const app = express();
 
 app.use(morgan('common'));
+app.use(bodyParser.json());
 
-let topTenMovies = [
+let users = [
+
+    {
+        id: 1,
+        name: 'Steve',
+        favoriteMovie: []
+    },
+
+    {
+        id: 2,
+        name: 'Tyler',
+        favoriteMovie: []
+    },
+
+    {
+        id: 3,
+        name: 'Sarah',
+        favoriteMovie: []
+    }
+];
+
+let movies = [
     
     {
         title: 'The Godfather',
-        director: 'Francis Ford Coppola'
+        genres: {
+            name: 'Crime',
+            description: 'Crime films, in the broadest sense, are a cinematic genre inspired by and analogous to the crime fiction literary genre.'
+        },
+        description: 'The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.',
+        directors: {
+            name: 'Francis Ford Coppola',
+            born: 'April 7th, 1939',
+            bio: 'Francis Ford Coppola is an American film director, producer, and screenwriter.'
+            
+        }
     },
 
     {
         title: 'The Shawshank Redemption',
-        director: 'Frank Darabont'
+        genres: {
+            name: 'Drama',
+            description: 'In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
+        },
+        description: 'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
+        directors: {
+            name: 'Frank Darabont',
+            born: 'January 28, 1959',
+            bio: 'Frank Árpád Darabont is a French-born American film director, screenwriter and producer.'
+        }
     },
 
     {
         title: 'Schindler\'s List',
-        director: 'Steven Spielberg'
+        genres: {
+            name: 'History',
+            description: 'A historical film is a fiction film showing past events or set within a historical period.'
+        },
+        description: 'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.',
+        directors: {
+            name: 'Steven Spielberg',
+            born: 'December 18, 1946',
+            bio: 'Steven Allan Spielberg KBE is an American filmmaker.'
+        }
     },
-
-    {
-        title: 'Raging Bull',
-        director: 'Martin Scorsese'
-    },
-
-    {
-        title: 'Casablanca',
-        director: 'Michael Curtiz'
-    },
-
-    {
-        title: 'Citizen Kane',
-        director: 'Orson Welles'
-    },
-
-    {
-        title: 'Gone with the Wind',
-        directors: 'Victor Fleming, George Cukor, Sam Wood'
-    },
-
-    {
-        title: 'The Wizard of Oz',
-        directors: 'Victor Fleming, George Cukor, Mervyn LeRoy, Norman Taurog, Richard Thorpe, King Vidor'
-    },
-
-    {
-        title: 'One Flew Over the Cuckoo\'s Nest',
-        director: 'Milos Forman'
-    },
-
-    {
-        title: 'Lawrence of Arabia',
-        director: 'David Lean'
-    }
-
 ];
 
 let logger = (req, res, next) => {
@@ -69,9 +85,116 @@ app.use(logger);
 app.get('/', (req, res) => {
     res.send('Welcome to myFlix!');
 });
-  
+ 
+// CREATE
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser);
+    } else {
+        res.status(400).send('Name field required.');
+    }
+});
+
+// UPDATE
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send('User not found.')
+    }
+}); 
+
+// CREATE
+app.post('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovie.push(movieTitle);
+        res.status(200).send(`${movieTitle} has been added to user's favorites`);
+    } else {
+        res.status(404).send('User not found.')
+    }
+}); 
+
+// DELETE 
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.favoriteMovie = user.favoriteMovie.filter(title => title!==movieTitle)
+        res.status(200).send(`${movieTitle} has been removed from user's favorites`);
+    } else {
+        res.status(404).send('User not found.')
+    }
+}); 
+
+// DELETE 
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        users = users.filter(user => user.id != id);
+        res.status(200).send(`User ${id} has been deleted`);
+    } else {
+        res.status(404).send('User not found.')
+    }
+}); 
+
+// READ
 app.get('/movies', (req, res) => {
-    res.json(topTenMovies);
+    res.status(200).json(movies);
+});
+
+// READ
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = movies.find(movie => movie.title === title);
+
+    if (movie) {
+        res.status(200).json(movie); 
+    } else {
+        res.status(404).send('Could not find that movie.');
+    }
+});
+
+// READ
+app.get('/movies/genres/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = movies.find(movie => movie.genres.name === genreName).genres;
+
+    if (genre) {
+        res.status(200).json(genre); 
+    } else {
+        res.status(404).send('Could not find that genre.');
+    }
+});
+
+// READ
+app.get('/movies/directors/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find(movie => movie.directors.name === directorName).directors;
+
+    if (director) {
+        res.status(200).json(director); 
+    } else {
+        res.status(404).send('Could not find that genre.');
+    }
 });
 
 app.use(express.static('public'));
